570ca88a70f04b3633d55b910f9998af5d7c463d
GuillemotClement <guillemot.clement@protonmail.com>
2026-02-15 10:37:32 +0100

    feat: add endpoint delete and detail project

--- front/src/pages/Project/ProjectDetailComponent.tsx
+++ front/src/pages/Project/ProjectDetailComponent.tsx
@@ -3,21 +3,25 @@ import { projectDetailRoute } from "../../router/router";
 import { api } from "../../libs/axios";
 
 export default function ProjectDetailComponent() {
-	// TanStack Router sait que projectId est une string grâce à la définition du path
-	const { projectId } = projectDetailRoute.useParams();
+  // TanStack Router sait que projectId est une string grâce à la définition du path
+  const { projectId } = projectDetailRoute.useParams();
 
-	// On utilise ensuite cet ID pour fetcher les données spécifiques
-	const { data: project, isLoading } = useQuery({
-		queryKey: ["projects", projectId],
-		queryFn: () => api.get(`/projects/${projectId}`).then((res) => res.data),
-	});
+  // On utilise ensuite cet ID pour fetcher les données spécifiques
+  const { data: project, isLoading } = useQuery({
+    queryKey: ["projects", projectId],
+    queryFn: async () => {
+      const response = await api.get(`/projects/${projectId}`);
+      return response.data.project;
+    },
+  });
 
-	if (isLoading) return <p>Chargement du projet {projectId}...</p>;
+  console.log(project);
+  if (isLoading) return <p>Chargement du projet {projectId}...</p>;
 
-	return (
-		<div>
-			<h1>{project.title}</h1>
-			<p>{project.description}</p>
-		</div>
-	);
+  return (
+    <div>
+      <h1>{project.title}</h1>
+      <p>{project.description}</p>
+    </div>
+  );
 }
